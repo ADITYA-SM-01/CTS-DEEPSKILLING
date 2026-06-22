b6a06dc19c8a0e0ee695a80e645ed3713bb56b66
ADITYA-SM-01 <smadityaofficial@gmail.com>
2026-06-22 22:19:22 +0530

    DSA-Recursion

---
+++ WEEK-1/MODULE-2_DataStructuresAndAlgorithms/FinancialForecast/src/FinancialForecast.java
@@ -0,0 +1,35 @@
+public class FinancialForecast {
+
+    public static double futureValue(double currentValue,
+                                     double growthRate,
+                                     int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Case
+        return futureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1
+        );
+    }
+
+    public static void main(String[] args) {
+
+        double presentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double predictedValue =
+                futureValue(presentValue, growthRate, years);
+
+        System.out.printf(
+                "Future Value after %d years = %.2f",
+                years,
+                predictedValue
+        );
+    }
+}
